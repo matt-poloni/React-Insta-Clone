492956fab6fba3426c78025386921e4f5b027ed8
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-25 19:02:57 -0400

    Gets moment.js working on timestamps

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,20 +3,22 @@ import './CommentSection.css';
 import Comment from './Comment';
 import PropTypes from 'prop-types';
 import { Form, Input } from 'reactstrap';
+import moment from 'moment';
 
 const CommentSection = props => {
   const comments = props.comments;
+  console.log(props.timestamp);
 
   return (
     <div className="post-comments">
       {comments.map(comment =>
         <Comment key={comment.id} comment={comment} />
       )}
-      <p className="post-time">{props.timestamp}</p>
+      <p className="post-time">{moment(props.timestamp,'MMMM Do YYYY, h:mm:ss a').fromNow()}</p>
       <div className="wrap-add-comment">
         <Form inline>
-            <Input type="text" className="add-comment" placeholder="Add a comment..." />
-          </Form>
+          <Input type="text" className="add-comment" placeholder="Add a comment..." />
+        </Form>
       </div>
     </div>
   )
@@ -29,7 +31,8 @@ CommentSection.propTypes = {
       username: PropTypes.string,
       text: PropTypes.string,
     })
-  )
+  ),
+  timestamp: PropTypes.string,
 }
 
 export default CommentSection;
