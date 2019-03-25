5d1284071cb1ecc276304956fb2c426dc1fb4273
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-25 17:25:54 -0400

    Begins setup for PostContainer

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './PostContainer.css';
 import CommentSection from '../CommentSection/CommentSection';
 
 const PostContainer = props => {
+  const post = props.post;
+
   return (
     <article className="post">
-      <div className="post-header">Post Header</div>
+      <div className="post-header">
+        <img src={post.thumbnailUrl} alt={post.username} />
+        <span className="username">{post.username}</span>
+      </div>
       <div className="post-img">Post Image</div>
       <div className="post-interact">Post Interactions</div>
       <CommentSection />
