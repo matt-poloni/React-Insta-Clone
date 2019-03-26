aa634e24af12e8f0a709b792d8d76d0ca223bda3
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-26 15:23:13 -0400

    Adds loading spinner before posts load & setTimeout to prove functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
+import { Spinner } from 'reactstrap';
 import dummyData from './dummy-data';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: dummyData,
+      posts: [],
     }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        posts: dummyData,
+      });
+    }, 2000);
   }
 
   render() {
@@ -17,8 +26,10 @@ class App extends Component {
       <div className="App">
         <SearchBar />
         <main className="wrap-posts">
-          {this.state.posts.map(post =>
-            <PostContainer key={post.id} post={post} />
+          {this.state.posts.length === 0
+            ? <Spinner className="load-main" />
+            : this.state.posts.map(post =>
+              <PostContainer key={post.id} post={post} />
           )}
         </main>
       </div>
