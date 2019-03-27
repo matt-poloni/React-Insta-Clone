32a1bdc812f5d491353d543bbf92d69c6e7bcfbc
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-27 16:14:58 -0400

    Finally refactors App into PostsPage, now fed into withAuthenticate

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,88 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
-import { Spinner } from 'reactstrap';
-import dummyData from './dummy-data';
+import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      posts: [],
-      search: '',
-      query: '',
-    }
+    this.state = {}
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        posts: dummyData,
-      });
-    }, 2000);
-  }
-
-  handleSearchChanges = e => {
-    this.setState({ search: e.target.value })
-  }
-
-  searchPosts = e => {
-    e.preventDefault();
-
-    this.setState({
-      query: this.state.search,
-    })
-  }
-
-  updatePostComments = (id, comments) => {
-    this.setState({
-      posts: this.state.posts.map(post => {
-        return post.id === id
-          ? {
-            ...post,
-            comments: comments,
-          }
-          : post;
-      })
-    })
-  }
-
-  toggleUserLike = (id, userLike, e) => {
-    this.setState({
-      posts: this.state.posts.map(post => {
-        return post.id === id
-          ? {
-            ...post,
-            userLike: !post.userLike,
-            likes: userLike ? post.likes-1 : post.likes+1,
-          }
-          : post;
-      })
-    })
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div className="App">
-        <SearchBar
-          searchPosts={this.searchPosts}
-          handleSearchChanges={this.handleSearchChanges}
-        />
-        <main className="wrap-posts">
-          {this.state.posts.length === 0
-            ? <Spinner className="load-main" />
-            : this.state.posts
-              .filter(post => post.username.includes(this.state.query))
-              .map(post =>
-                <PostContainer
-                  key={post.id}
-                  post={post}
-                  updatePostComments={this.updatePostComments}
-                  toggleUserLike={this.toggleUserLike}
-                />
-          )}
-        </main>
+        <ComponentFromWithAuthenticate />
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostsPage.js
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from './PostContainer';
+import { Spinner } from 'reactstrap';
+import dummyData from '../../dummy-data';
 
-const PostsPage = props => {
-  return ()
+class PostsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      search: '',
+      query: '',
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        posts: dummyData,
+      });
+    }, 2000);
+  }
+
+  handleSearchChanges = e => {
+    this.setState({ search: e.target.value })
+  }
+
+  searchPosts = e => {
+    e.preventDefault();
+
+    this.setState({
+      query: this.state.search,
+    })
+  }
+
+  updatePostComments = (id, comments) => {
+    this.setState({
+      posts: this.state.posts.map(post => {
+        return post.id === id
+          ? {
+            ...post,
+            comments: comments,
+          }
+          : post;
+      })
+    })
+  }
+
+  toggleUserLike = (id, userLike, e) => {
+    this.setState({
+      posts: this.state.posts.map(post => {
+        return post.id === id
+          ? {
+            ...post,
+            userLike: !post.userLike,
+            likes: userLike ? post.likes-1 : post.likes+1,
+          }
+          : post;
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchBar
+          searchPosts={this.searchPosts}
+          handleSearchChanges={this.handleSearchChanges}
+        />
+        <main className="wrap-posts">
+          {this.state.posts.length === 0
+            ? <Spinner className="load-main" />
+            : this.state.posts
+              .filter(post => post.username.includes(this.state.query))
+              .map(post =>
+                <PostContainer
+                  key={post.id}
+                  post={post}
+                  updatePostComments={this.updatePostComments}
+                  toggleUserLike={this.toggleUserLike}
+                />
+          )}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default PostsPage;
