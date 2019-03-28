54b48d0b5082a7e6aded559408710321564d0d0a
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-28 16:17:00 -0400

    Refactors SearchBar to better use styled components

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { Form, Input } from 'reactstrap';
 
 const Header = styled.header`
   position: fixed;
@@ -13,62 +12,54 @@ const Header = styled.header`
   border-bottom: 1px solid lightgray;
   background-color: white;
   z-index: 2;
+`
 
-  .flex-searchbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .logo-ig {
-    font-family: 'Lobster Two', cursive;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-  }
-  
-  .logo-ig i {
+const LogoHeader = styled.div`
+  font-family: 'Lobster Two', cursive;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  i {
     margin-right: 0.5em;
     padding-right: 0.5em;
     border-right: 1px solid lightgray;
   }
-  
-  .search {
-    display: flex;
-    align-items: center;
-  }
-  
-  .search input {
-    text-align: center;
-    background-color: #FAFAFA;
-  }
-  
-  .nav-site i {
-    margin-left: 1em;
-  }
 `
+
+const SearchHeader = styled.form`
+  display: flex;
+  align-items: center;
+`
+
+const SearchInput = styled.input`
+  font-size: 1.4rem;
+  text-align: center;
+  background-color: #FAFAFA;
+  border: 1px solid lightgrey;
+  border-radius: 0.5rem;
+`
+
+const NavHeader = styled.div` i { margin-left: 1em } `
 
 const SearchBar = props => {
   return (
     <Header>
-      <div className="logo-ig">
+      <LogoHeader>
         <i className="fab fa-instagram"></i>
         <h1 className="wordmark-ig">Instagram</h1>
-      </div>
-      <div className="search">
-        <Form inline
-          onSubmit={props.searchPosts}
-        >
-          <Input
-            type="search"
-            id="searchbar"
-            placeholder="Search"
-            bsSize="lg"
-            onChange={props.handleSearchChanges}
-          />
-        </Form>
-      </div>
-      <div className="nav-site">
+      </LogoHeader>
+      <SearchHeader
+        onSubmit={props.searchPosts}
+      >
+        <SearchInput
+          type="search"
+          id="searchbar"
+          placeholder="Search"
+          onChange={props.handleSearchChanges}
+        />
+      </SearchHeader>
+      <NavHeader>
         <i className="far fa-compass"></i>
         <i className="far fa-heart"></i>
         <i
@@ -76,7 +67,7 @@ const SearchBar = props => {
           onClick={props.logout}
           title={props.username}
         ></i>
-      </div>
+      </NavHeader>
     </Header>
   )
 }
