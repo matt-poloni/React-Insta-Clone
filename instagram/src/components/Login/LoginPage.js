9277acaf94709b5537eedb487d709e11fc523c2d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-03-28 15:56:46 -0400

    Moves Login.css to Login.js

--- instagram/src/components/Login/LoginPage.js
+++ instagram/src/components/Login/LoginPage.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 import './Login.css';
 import { Form, Input, Button } from 'reactstrap';
+
+const WrapLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  width: 35rem;
+  background-color: white;
+  border: 1px solid lightgray;
+  border-radius: 0.5rem;
+  margin: 1rem 0;
+  padding: 4rem;
+
+  .login-header {
+    font-family: 'Lobster Two', cursive;
+    font-size: 4rem;
+    margin-bottom: 3rem;
+  }
+  
+  .login-form input {
+    background-color: #FAFAFA;
+    margin-top: 1rem;
+  }
+  
+  .login-form button {
+    margin-top: 2rem;
+  }
+`
 
 class LoginPage extends React.Component {
   constructor(props) {
@@ -24,7 +52,7 @@ class LoginPage extends React.Component {
 
   render() {
     return (
-      <div className="wrap-login">
+      <WrapLogin>
         <header className="wrap-login-header">
           <h1 className="login-header">Instagram</h1>
         </header>
@@ -50,7 +78,7 @@ class LoginPage extends React.Component {
           />
           <Button type="submit" color="primary" size="lg" block>Login</Button>
         </Form>
-      </div>
+      </WrapLogin>
     )
   }
 }
